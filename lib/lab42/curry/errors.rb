bc3efdfa221c642f45cc9ab68d615f5800d0c0d7
RobertDober <robert.dober@gmail.com>
2020-12-28 15:38:01 +0100

    Fixes: #8;

--- lib/lab42/curry/errors.rb
+++ lib/lab42/curry/errors.rb
@@ -1,7 +1,8 @@
 module Lab42
   module Curry
-    DuplicatePositionSpecification = Class.new RuntimeError
+    DuplicateBlock                 = Class.new RuntimeError
     DuplicateKeywordArgument       = Class.new RuntimeError
+    DuplicatePositionSpecification = Class.new RuntimeError
     MissingRuntimeArg              = Class.new RuntimeError
   end
 end
